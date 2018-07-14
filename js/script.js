b4e4da10d83ed511917b8447c780d714ed04a62d
Nick <niiickolay@gmail.com>
2018-07-14 18:24:44 +0300

    modal is alive

--- js/script.js
+++ js/script.js
@@ -3,8 +3,35 @@ const mainMenu = document.querySelector(".main-nav");
 
 mainMenu.classList.remove("main-nav--no-js");
 
-function showMenu() {
+toggle.addEventListener("click", function (evt) {
+  evt.preventDefault();
   mainMenu.classList.toggle("main-nav--closed");
+});
+
+
+const modal = document.querySelector(".modal");
+const button = document.querySelector(".good__order-button");
+const focusField = document.querySelector(".add-cort__size-input");
+
+
+function showModal(a) {
+  a.preventDefault();
+  modal.classList.add("modal--shown");
+  focusField.focus();
+
+
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27 && modal.classList.contains("modal--shown")) {
+      modal.classList.remove("modal--shown");
+    }
+  });
+
+  window.addEventListener("click", function(evt) {
+    if (evt.target == modal) {
+      modal.classList.remove("modal--shown");
+    }
+  });
 }
 
-toggle.addEventListener("click", showMenu);
+
+button.addEventListener("click", showModal);
